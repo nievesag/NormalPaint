2678c54d286128914061f0f6afe0e51fd63ca834
nievesag <nievalon@ucm.es>
2026-06-02 00:13:48 +0200

    compute compila

--- NormalPaint/materials/shaders/compute_shader.glsl
+++ NormalPaint/materials/shaders/compute_shader.glsl
@@ -2,8 +2,8 @@
 #version 450
 
 layout(set = 0, binding = 0, std430) readonly buffer parameters {
-    float w;
-    float h;
+    float width;
+    float height;
     float mask_w;
     float mask_h;
     float cx;
@@ -24,32 +24,33 @@ layout(local_size_x = 8, local_size_y = 8, local_size_z = 1) in;
 void main() {
     ivec2 local = ivec2(gl_GlobalInvocationID.xy);
 
-    int x = cx - radius + local.x;
-    int y = cy - radius + local.y;
+    float x = float(params.cx - params.radius + local.x);
+    float y = float(params.cy - params.radius + local.y);
 
-    int px = ((x % w) + w) % w;
-    int py = ((y % h) + h) % h;
+    int px = int(mod( ( mod(x, float(params.width)) + float(params.width)), float(params.width) ));
+    int py = int(mod( ( mod(y, float(params.height)) + float(params.height)), float(params.height) ));
 
     // normalizamos la pos local dentro de 0-1
 	// max evita dividir entre 0 si el diametro fuese 1
-    float u = clamp(float(local.x) / float(max(1, diameter - 1)), 0.0, 1.0);
-    float v = clamp(float(local.y) / float(max(1, diameter - 1)), 0.0, 1.0);
+    float u = clamp(float(local.x) / float(max(1, params.diameter - 1)), 0.0, 1.0);
+    float v = clamp(float(local.y) / float(max(1, params.diameter - 1)), 0.0, 1.0);
 
     // convertimos esa posición normalizada a coordenadas reales dentro de la máscara del brush
-	// si u = 0.0 → mx = 0
-	// si u = 1.0 → mx = mask_w - 1 
-    int mx = int(floor((u * float(mask_w - 1)) + 0.5));
-	int my = int(floor((v * float(mask_h - 1)) + 0.5));
+	// si u = 0.0 -> mx = 0
+	// si u = 1.0 -> mx = mask_w - 1 
+    int mx = int(floor((u * float(params.mask_w - 1)) + 0.5));
+	int my = int(floor((v * float(params.mask_h - 1)) + 0.5));
 
-    float mask_value = clamp(imageLoad(image, local).r, 0.0, 1.0) * brush_strength;
+    float mask_value = clamp(imageLoad(image, local).r, 0.0, 1.0) * params.brush_strength;
 
     if (mask_value <= 0.0)
     {
-        discard;
+        return;
     }
 
-    vec4 base = imageLoad(image_1, vec2(px, py));
-    imageStore(image_1, vec2(px, py), mix(base, color, mask_value));
+    vec4 base = imageLoad(image_1, ivec2(px, py));
+    vec4 output_color = mix(base, params.color, mask_value);
+    imageStore(image_1, ivec2(px, py), output_color);
 
     //vec4 color = imageLoad(image, uv);
     //vec4 multiplier = vec4(params.r, params.g, params.b, 1.0);
